3b59367b0101513c87d945e37cfe3fa5ffdb54a0
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-02-08 17:34:41 -0600

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { AddCategory, GifGrid } from './components/';
+
+const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState([ 'Jujutsu Kaisen' ]);
+
+  const onAddCategory = ( newCategory ) => {
+    if(categories.includes(newCategory)) return;
+
+    setCategories([ newCategory, ...categories ]);
+    // setCategories( cat => [newCategory, ...cat] );
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory 
+        onNewCategory={ (value) => onAddCategory(value) } 
+      />
+
+      { categories.map(category => (
+          <GifGrid key={category} category={category} />
+        )) 
+      }
+
+    </>
+  );
+};
+
+export default GifExpertApp;
